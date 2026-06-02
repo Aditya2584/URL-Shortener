93830b01daeaadc43967b4248c17b4328ecd678e
Aditya2584 <bestrong2584@gmail.com>
2026-06-02 13:44:21 +0530

    jwt token

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,20 @@
-const sessionIdToUseMap = new Map();
+const jwt = require('jsonwebtoken')
+const secret = "Aditya$123@$";
 
-function setUser(id, user){
-    sessionIdToUseMap.set(id, user)
+function setUser(user){
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    }, secret);
 }
 
-function getUser(id){
-    return sessionIdToUseMap.get(id); 
+function getUser(token){
+    if(!token) return null;
+    try{
+        return jwt.verify(token, secret)
+    }catch(error){
+        return null;
+    }
 }
 
 module.exports = {
